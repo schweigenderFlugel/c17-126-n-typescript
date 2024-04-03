9fb50b96aee2819e1a0512470111c4d42ba240c9
Saúl Belbey <belbeysaul@hotmail.com>
2024-04-03 00:07:59 -0300

    feat: distBE deleted

--- src/backend/distBE/services/user.services.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class userServices {
-    static async createUser() {
-        // TODO: Create user connecting to the DAO
-    }
-}
-exports.default = userServices;
