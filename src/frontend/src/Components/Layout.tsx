8ca5491c1f18d552ffbd13f20f31106cadef9b1e
schweigenderFlugel <facundoezcastro@gmail.com>
2024-04-04 16:04:12 -0300

    add dark mode, create navbar component and logout hook

--- src/frontend/src/Components/Layout.tsx
+++ src/frontend/src/Components/Layout.tsx
@@ -1,6 +1,6 @@
 export const Layout = ({ children }) => {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-gradient-to-br from-indigo-300 from-20% via-white via-50% to-indigo-300 to-95%">
+    <div className="h-screen w-screen bg-gradient-to-br from-indigo-300 dark:from-indigo-800 from-20% via-white dark:via-black via-50% to-indigo-300 dark:to-indigo-800 to-95%">
       {children}
     </div>
   )
--- src/frontend/src/Pages/SignUp.tsx
+++ src/frontend/src/Pages/SignUp.tsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AxiosError } from "axios";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 import { signup } from "../Services/user";
 import { Modal } from "../Components/Modal";
 import { ErrorMessage } from "../Components/ErrorMessage";
 
 
 export const SignUp = () => {
+  const [ fullname, setFullname ] = useState<string>('');
   const [ email, setEmail ] = useState<string>('');
   const [ password, setPassword ] = useState<string>('');
   const [ confirmPassword, setConfirmPassword ] = useState<string>('');
+  const [ showPassword, setShowPassword ] = useState<boolean>(false);
+  const [ showConfirmPassword, setShowConfirmPassword ] = useState<boolean>(false);
   const [ openModal, setOpenModal ] = useState<boolean>(false);
   const [ error, setError ] = useState<string>('');
   const navigate = useNavigate();
@@ -27,55 +31,103 @@ export const SignUp = () => {
     }
   }
 
+  const passwordInputType = showPassword ? "text" : "password";
+  const confirmPasswordInputType = showConfirmPassword ? "text" : "password";
+
   return (
     <>
       <Modal open={openModal}>
         <ErrorMessage>{error}</ErrorMessage>
       </Modal>
-      <main className="grid justify-center items-center">
-        <header className="text-[18px] font-bold">Registro</header>
-        <form action="" onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">Email</label>
+      <main className="inline-grid justify-items-center p-12 bg-white dark:bg-black bg-opacity-25 dark:bg-opacity-25 rounded-2xl border border-indigo-500">
+        <h1 className="text-[28px] text-black dark:text-white font-bold text-center">Registro</h1>
+        <h2 className="text-center dark:text-white mb-4">Nos alegra tenerte por acá</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label htmlFor="fullname" className="text-black dark:text-white">Nombre completo</label>
             <input 
               type="text" 
-              id="email" 
-              name="email" 
+              id="fullname"
+              name="fullname"
+              className="w-full h-8 rounded-md px-2"
+              onChange={(e) => setFullname(e.target.value)} 
+              value={fullname}
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="email" className="text-black dark:text-white">Email</label>
+            <input 
+              type="text" 
+              id="email"
+              name="email"
+              className="w-full h-8 rounded-md px-2"
               onChange={(e) => setEmail(e.target.value)} 
               value={email}
             />
           </div>
-          <div>
-            <label htmlFor="password">Contraseña</label>
-            <input 
-              type="password" 
-              id="password" 
-              name="password" 
-              onChange={(e) => setPassword(e.target.value)} 
-              value={password} 
-            />
+          <div className="mb-6">
+            <label htmlFor="password" className="text-black dark:text-white">Contraseña</label>
+            <div className="relative flex justify-between items-center">
+              <input 
+                type={passwordInputType} 
+                id="password" 
+                name="password"
+                className="w-full h-8 rounded-md px-2"
+                onChange={(e) => setPassword(e.target.value)} 
+                value={password} 
+              />
+              {showPassword 
+                ? <BsEyeSlash 
+                    className="absolute right-3 cursor-pointer hover:text-indigo-500"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+                : <BsEye 
+                    className="absolute right-3 cursor-pointer hover:text-indigo-500"
+                    onClick={() => setShowPassword(!showPassword)}
+                  />
+              }
+            </div>
+          </div>
+          <div className="mb-6">
+            <label htmlFor="confirm-password" className="text-black dark:text-white">Confirmar Contraseña</label>
+            <div className="relative flex justify-between items-center">
+              <input 
+                type={confirmPasswordInputType}
+                id="confirm-password" 
+                name="confirm-password"
+                className="w-full h-8 rounded-md px-2"
+                onChange={(e) => setConfirmPassword(e.target.value)} 
+                value={confirmPassword} 
+              />
+              {showConfirmPassword 
+                ? <BsEyeSlash 
+                    className="absolute right-3 cursor-pointer hover:text-indigo-500"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  />
+                : <BsEye 
+                    className="absolute right-3 cursor-pointer hover:text-indigo-500"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  />
+              }
+            </div>
           </div>
           <div>
-            <label htmlFor="confirm-password">Confirmar Contraseña</label>
-            <input 
-              type="password" 
-              id="confirm-password" 
-              name="confirm-password" 
-              onChange={(e) => setConfirmPassword(e.target.value)} 
-              value={confirmPassword} 
-            />
+            <button
+              type="submit"
+              className="w-full text-white bg-indigo-600 px-2 py-1 cursor-pointer rounded-lg enabled:hover:bg-indigo-500 disabled:opacity-50 disabled:cursor-default"
+              disabled={
+                fullname === '' ||
+                email === '' ||
+                password === '' ||
+                confirmPassword === ''
+              }
+            >
+              Registrarse
+            </button>
           </div>
-          <button
-            type="submit"
-            className="text-white bg-green-700 px-2 py-1 cursor-pointer rounded-lg disabled:opacity-50 disabled:cursor-default"
-            disabled={
-              email === '' && 
-              password === '' &&
-              confirmPassword === ''
-            }
-          >
-            Registrar
-          </button>
+          <div className="text-center text-black dark:text-white">
+            <p>¿Ya tienes cuenta? <Link to={"/login"}>Inicia sesión</Link></p>
+          </div>
         </form>
       </main>
     </>
