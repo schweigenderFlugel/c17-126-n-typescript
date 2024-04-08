7aa97517322bce560dce64f1853ebc505b7f5407
Alexis Checura <alexischecura@gmail.com>
2024-04-08 12:22:49 -0300

    feat: add new route structure

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,30 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
 import { AuthProvider } from './Context/AuthContext';
+
+import { LandingPage } from './Pages/LandingPage';
 import { SignUp } from './Pages/SignUp';
-import "./index.css";
 import { Login } from './Pages/Login';
-import { Navbar } from './Components/Navbar';
+
 import { Layout } from './Components/Layout';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-      <Layout>
-        <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Landing Page</h1>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registro" element={<SignUp/>} />
-          {/* 🔏 RUTAS PRIVADAS 👇*/}
-          <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-          <Route path="/transferencias" element={<h1>Transferencias</h1>} />
-          <Route path="/inversiones" element={<h1>Inversiones</h1>} />
-        </Routes> 
-      </Layout>
-    </BrowserRouter>
+          <Route path="/" element={<LandingPage />} />
+          <Route element={<Layout />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/registro" element={<SignUp />} />
+            {/* 🔏 RUTAS PRIVADAS 👇*/}
+            <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+            <Route path="/transferencias" element={<h1>Transferencias</h1>} />
+            <Route path="/inversiones" element={<h1>Inversiones</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
-
 export default App;
