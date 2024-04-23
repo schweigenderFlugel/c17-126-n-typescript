21e7c840dd76772b296df66615e684bde18fed9d
Alexis Checura <alexischecura@gmail.com>
2024-04-22 21:12:40 -0300

    feat: add styles to dashboard start

--- frontend/src/Components/BalanceCard.tsx
+++ frontend/src/Components/BalanceCard.tsx
@@ -1,29 +1,41 @@
-import { HistorialGraph } from "./HistorialGraph"
+import { HistorialGraph } from './HistorialGraph';
 
 export const BalanceCard = () => {
   return (
     <section className="col-span-8">
-      <main className="grid grid-rows-10 gap-3">
-        <h2 className="row-span-1 font-bold dark:text-white">Balance</h2>
-        <div className="row-span-2 grid grid-cols-3 gap-2">
-          <div className="col-span-1 p-2 rounded-xl bg-white dark:bg-black dark:bg-opacity-70">
+      <main className="gap-3 grid overflow-hiddenF">
+        <h3 className="font-semibold text-2xl dark:text-white">Balance</h3>
+        <div className="gap-2 grid grid-cols-3 min-w-[500px]">
+          <div className="border-indigo-600 col-span-1 bg-indigo-100 dark:bg-indigo-900 dark:bg-opacity-70 shadow-md p-4 border rounded-xl">
             <p className="dark:text-white">Balance Total</p>
-            <p className="dark:text-white">$5000</p>
+            <p className="font-semibold text-3xl dark:text-white">$5000</p>
           </div>
-          <div className="col-span-1 p-2 rounded-xl bg-white dark:bg-black dark:bg-opacity-70">
-            <p className="dark:text-white">Gastos</p>
-            <p className="dark:text-white">$500</p>
+          <div className="border-indigo-600 col-span-1 bg-indigo-100 dark:bg-indigo-900 dark:bg-opacity-70 shadow-md p-4 border rounded-xl">
+            <p className="font-medium dark:text-white">Gastos</p>
+            <p className="font-semibold text-3xl dark:text-white">$1500</p>
           </div>
-          <div className="col-span-1 p-2 rounded-xl bg-white dark:bg-black dark:bg-opacity-70">
-            <p className="dark:text-white">Cuenta</p>
-            <p className="dark:text-white">$800.02</p>
+          <div className="border-indigo-600 col-span-1 bg-indigo-100 dark:bg-indigo-900 dark:bg-opacity-70 shadow-md p-4 border rounded-xl">
+            <p className="font-medium dark:text-white">Cuenta</p>
+            <p className="font-semibold text-3xl dark:text-white">$3500</p>
           </div>
         </div>
-        <div className="row-span-1 dark:text-white text-end">Anual</div>
-        <div className="row-span-6 rounded-xl bg-white dark:bg-black">
+        <div className="flex border-indigo-300 row-span-1 bg-indigo-100 dark:bg-indigo-900 ml-auto border rounded-lg w-fit text-end text-sm dark:text-white">
+          <button className="hover:bg-indigo-200 dark:hover:bg-indigo-800 p-2 transition-colors">
+            Anual
+          </button>
+          <span className="bg-indigo-300 my-auto w-[1px] h-[80%]"></span>
+          <button className="hover:bg-indigo-200 dark:hover:bg-indigo-800 p-2 transition-colors">
+            Mensual
+          </button>
+          <span className="bg-indigo-300 my-auto w-[1px] h-[80%]"></span>
+          <button className="hover:bg-indigo-200 dark:hover:bg-indigo-800 p-2 transition-colors">
+            Semanal
+          </button>
+        </div>
+        <div className="row-span-6 bg-indigo-100 dark:bg-black rounded-xl">
           <HistorialGraph />
         </div>
       </main>
     </section>
-  )
-}
+  );
+};
--- frontend/src/Components/WalletCard.tsx
+++ frontend/src/Components/WalletCard.tsx
@@ -1,10 +1,64 @@
+import { useState } from 'react';
+import { Logo } from './Logo';
+import { HiOutlineEye, HiOutlineEyeSlash } from 'react-icons/hi2';
+
 export const WalletCard = () => {
+  const [showBalance, setShowBalance] = useState(true);
+
+  //👇 Obtenerlo del hook useAuth
+  const currentUser = {
+    firstName: 'Juan Alberto',
+    lastName: 'Perez',
+    balance: '3500.00',
+  };
+
   return (
     <section className="col-span-4">
-      <main className="grid grid-rows-10 gap-3">
-        <h2 className="font-bold dark:text-white">Billetera</h2>
-        <img src="public/img/Card.jpg" alt="Card" className="row-span-5"/>
+      <main>
+        <h3 className="font-semibold text-2xl dark:text-white">Billetera</h3>
+        <div className="bg-indigo-700 p-6 rounded-xl w-[340px] h-[216px]">
+          <div className="flex items-center">
+            <svg
+              width="69"
+              height="40"
+              viewBox="0 0 69 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                width="40"
+                height="40"
+                rx="20"
+                fill="white"
+                fill-opacity="0.3"
+              />
+              <rect
+                x="29"
+                width="40"
+                height="40"
+                rx="20"
+                fill="white"
+                fill-opacity="0.3"
+              />
+            </svg>
+            <Logo extraSmall className="ml-auto text-white" />
+          </div>
+
+          <p className="mt-12 mb-3 text-lg text-white uppercase tracking-[4px]">{`${currentUser.firstName} ${currentUser.lastName}`}</p>
+          <div className="flex text-white">
+            <p className="min-w-44 font-semibold text-3xl tracking-wider">
+              $ {showBalance ? currentUser.balance : `●●●●.●●`}
+            </p>
+            <button onClick={() => setShowBalance(show => !show)}>
+              {showBalance ? (
+                <HiOutlineEye className="w-6 h-6" />
+              ) : (
+                <HiOutlineEyeSlash className="w-6 h-6" />
+              )}
+            </button>
+          </div>
+        </div>
       </main>
     </section>
-  )
-}
+  );
+};
